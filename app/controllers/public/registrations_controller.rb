ad6435b530e2ac6e8f517d8ac4ce37df056c9ad1
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-05 06:39:31 +0000

    [Finish]会員登録機能、ログイン機能、ヘッダー及びフッターの作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :users_permitted_parameteres, if: :devise_controller?
+  
+  def after_sign_up_path_for(resource)
+    posts_path
+  end
+  
+  protected
+  
+  def users_permitted_parameteres
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
