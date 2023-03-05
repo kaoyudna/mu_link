dc9e6b491179c72b259839715261f2ee95cb98d5
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-05 06:43:38 +0000

    [Add]ユーザー名のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  #ユーザー名の文字数
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  
 end
