a5725d2036408c6b0f2fd191b439793506dc60d9
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-19 09:20:20 +0000

    [Add]

--- db/migrate/20230319085730_add_details_to_post_comments.rb
+++ db/migrate/20230319085730_add_details_to_post_comments.rb
@@ -1,5 +1,5 @@
 class AddDetailsToPostComments < ActiveRecord::Migration[6.1]
   def change
-    add_column :post_comments, :is_deleted, :boolean
+    add_column :post_comments, :is_deleted, :boolean, default: false, null: false
   end
 end
