8deb8e94e9d091ada01dbda4f59c64fc799e81bf
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-19 10:01:24 +0000

    [Add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_042717) do
+ActiveRecord::Schema.define(version: 2023_03_19_085730) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
