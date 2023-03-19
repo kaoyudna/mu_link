a845f6c6ba4e30a7f2a3f67796c35efecfcc3ba6
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-19 11:53:40 +0000

    [Fix]レイアウト修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -58,7 +58,7 @@ class Public::PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_path, notice: '投稿を削除しました'
+    redirect_to request.referer, notice: '投稿を削除しました'
   end
 
   private
