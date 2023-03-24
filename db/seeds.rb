9fca11eb1cb5d70467589e2e47c55051517de51a
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-03-24 07:23:31 +0000

    [Add]seedファイルの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,62 @@ Admin.create!(
 
 # ユーザー
 [
-  ['misaki@example.com', 'Misaki', 'mu_link'],
-  ['akihiro@example.com', 'Akihiro', 'mu_link'],
-  ['haruki@example.com', 'Haruki', 'mu_link'],
-  ['ryusei@example.com', 'Ryusei', 'mu_link'],
-  ['saori@example.com', 'Saori', 'mu_link'],
-  ['daigo@example.com', 'Daigo', 'mu_link'],
-  ['yua@example.com', 'Yua', 'mu_link'],
-  ['souta@example.com', 'Souta', 'mu_link'],
-  ['kaede@example.com', 'Kaede', 'mu_link'],
-  ['rion@example.com', 'Rion', 'mu_link'],
-  ['minato@example.com', 'Minato', 'mu_link'],
-  ['saki@example.com', 'Saki', 'mu_link'],
-  ['aoi@example.com', 'Aoi', 'mu_link'],
-  ['eito@example.com', 'Eito', 'mu_link'],
-].each do |mail, name, password|
-  User.create!(
-    { email: mail, name: name, password: password}
-  )
+  ['1','misaki@example.com', 'Misaki', 'mu_link'],
+  ['2','akihiro@example.com', 'Akihiro', 'mu_link'],
+  ['3','haruki@example.com', 'Haruki', 'mu_link'],
+  ['4','ryusei@example.com', 'Ryusei', 'mu_link'],
+  ['5','saori@example.com', 'Saori', 'mu_link'],
+  ['6','daigo@example.com', 'Daigo', 'mu_link'],
+  ['7','yua@example.com', 'Yua', 'mu_link'],
+  ['8','souta@example.com', 'Souta', 'mu_link'],
+  ['9','kaede@example.com', 'Kaede', 'mu_link'],
+  ['10','rion@example.com', 'Rion', 'mu_link'],
+  ['11','minato@example.com', 'Minato', 'mu_link'],
+  ['12','saki@example.com', 'Saki', 'mu_link'],
+  ['13','aoi@example.com', 'Aoi', 'mu_link'],
+  ['14','eito@example.com', 'Eito', 'mu_link'],
+].each do |id, mail, name, password|
+    User.create!(
+      { id: id, email: mail, name: name, password: password}
+    )
   end
 
+
 #ジャンル
-genres = %w(邦楽 ポップス(邦楽) ヒップホップ(邦楽) ロック(邦楽) ダンス(邦楽) 洋楽 ポップス(洋楽) ヒップホップ(洋楽) ロック(洋楽) ダンス(洋楽))
+genres = %w(ポップス(邦楽) ヒップホップ(邦楽) ロック(邦楽) ダンス(邦楽) ポップス(洋楽) ヒップホップ(洋楽) ロック(洋楽) ダンス(洋楽))
 genres.each do |genre|
   Genre.find_or_create_by(name: genre)
 end
+
+#ユーザージャンル
+[
+  ['1','1'],
+  ['2','1'],
+  ['3','1'],
+  ['4','1'],
+  ['5','8'],
+  ['6','8'],
+  ['7','8'],
+  ['9','8'],
+  ['1','2'],
+  ['5','2'],
+  ['10','2'],
+  ['11','2'],
+  ['12','2'],
+  ['13','2'],
+].each do |user_id, genre_id|
+    UserGenre.create!(
+      { user_id: user_id, genre_id: genre_id}
+      )
+  end
+
+#アーティストのいいね
+[
+  ['1','5kVZa4lFUmAQlBogl1fkd6'],
+  ['2','5kVZa4lFUmAQlBogl1fkd6'],
+  ['3','1snhtMLeb2DYoMOcVbb8iB'],
+].each do |user_id, artist|
+    ArtistFavorite.create!(
+      { user_id: user_id, artist_id: artist }
+      )
+  end
