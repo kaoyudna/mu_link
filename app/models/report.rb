453877a455e7470bd0c364981ed585613b596629
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-04-04 11:06:32 +0000

    [Add]中断

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,10 @@
 class Report < ApplicationRecord
+
+  default_scope -> {order(created_at: :desc)}
+  #reporter => 通報者
+  belongs_to :reporter, class_name: "User"
+  #reported => 通報される人
+  belongs_to :reported, class_name: "User"
+
+  enum status: {waiting: 0, keep: 1, finish: 2}
 end
